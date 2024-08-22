5e71abe8569c995455cd3e2eaa56e28a5f468556
TrifonovRoman <gromtr3@gmail.com>
2024-08-22 15:45:56 +0800

    create function for testing

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from tesseract_reader.image_reader import ImageReader
 from tesseract_reader.tesseract_reader import TesseractReader, TesseractReaderConfig
-reader_img = ImageReader()
-config = TesseractReaderConfig(lang="rus")
-reader = TesseractReader(config)
+def get_bboxes():
+    reader_img = ImageReader()
+    config = TesseractReaderConfig(lang="rus")
+    reader = TesseractReader(config)
 
-img = reader_img.read('example_image/img_1.jpeg')
-bboxes = reader.read(img)
-print(bboxes)
+    img = reader_img.read('example_image/img_1.jpeg')
+    bboxes = reader.read(img)
+    return bboxes
+
+if __name__ == "__main__":
+    bboxes = get_bboxes()
+    print(bboxes)
